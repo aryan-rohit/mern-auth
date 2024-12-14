731602dc349d268f5a892b374fc252be4f205f64
aryan-rohit <aryan.skumar.mec21@itbhu.ac.in>
2024-12-14 15:25:37 +0530

    completed SignUp function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-export const signup= async (req,res)=>{
+export const signup= async (req,res,next)=>{
     // console.log(req.body);
     const {username,email,password}=req.body;
     const hashedPassword=bcryptjs.hashSync(password,10); //number of round for salt 10
@@ -10,7 +10,7 @@ export const signup= async (req,res)=>{
         res.status(201).json({message:"User created succesfully"});
     }
     catch(error){
-        res.status(500).json(error.message);
+        next(error);
     }
     
 
